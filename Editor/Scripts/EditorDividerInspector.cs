040b0b1848217fc7c73f8b50a6745cff2438b103
Jaris Rener <jaris@leagueofgeeks.com>
2023-02-26 21:48:39 +1100

    Initial commit

---
+++ Editor/Scripts/EditorDividerInspector.cs
@@ -0,0 +1,112 @@
+﻿namespace HierarchyExtensions
+{
+    using System;
+    using System.Linq;
+    using UnityEditor;
+    using UnityEngine;
+
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(EditorDivider))]
+    public class EditorDividerInspector
+        : Editor
+    {
+        private static readonly Color[] _palette =
+        {
+            new(0.101f, 0.737f, 0.611f),
+            new(0.180f, 0.800f, 0.443f),
+            new(0.203f, 0.596f, 0.858f),
+            new(0.607f, 0.349f, 0.713f),
+            new(0.203f, 0.286f, 0.368f),
+            new(0.945f, 0.768f, 0.058f),
+            new(0.901f, 0.494f, 0.133f),
+            new(0.905f, 0.298f, 0.235f),
+            new(0.925f, 0.941f, 0.945f),
+            new(0.584f, 0.647f, 0.650f),
+        };
+
+        private static string[] _iconOptions =
+        {
+            "d_Favorite",
+            "d_Project",
+            "d_FilterByLabel",
+            "d_FilterByType",
+            "d_Settings",
+            "d_SceneViewCamera",
+            "d_Lighting",
+            "d_Profiler.UIDetails",
+            "d_PreMatCube",
+            "d_PreMatSphere",
+            "d_Profiler.Audio",
+            "d_Font Icon"
+        };
+
+        public override void OnInspectorGUI()
+        {
+            var divider = (EditorDivider) target;
+
+            EditorGUILayout.BeginHorizontal();
+
+            var selectedIndex = Array.IndexOf(_iconOptions, divider.Icon);
+            if (selectedIndex < 0)
+            {
+                selectedIndex = 0;
+                divider.Icon = _iconOptions[selectedIndex];
+                EditorUtility.SetDirty(divider);
+            }
+
+            var button = new GUIStyle("RL FooterButton")
+            {
+                margin = new RectOffset(0, 0, 2, 0),
+                fixedWidth = 24,
+                fixedHeight = EditorGUIUtility.singleLineHeight,
+            };
+
+            var content = new GUIContent(EditorGUIUtility.IconContent(_iconOptions[selectedIndex]));
+            content.tooltip = "Change divider icon";
+            if (GUILayout.Button(content, button))
+            {
+                if (_iconOptions.Length - 1 <= selectedIndex)
+                    selectedIndex = 0;
+                else
+                    ++selectedIndex;
+
+                Undo.RecordObject(divider, "Change Divider Icon");
+                divider.Icon = _iconOptions[selectedIndex];
+                EditorUtility.SetDirty(divider);
+            }
+
+            EditorGUI.BeginChangeCheck();
+            var color = EditorGUILayout.ColorField(divider.Color);
+
+            var rect = GUILayoutUtility.GetLastRect();
+            var w = rect.width;
+            rect.y = rect.yMax + 2;
+            rect.height = 16;
+            ++rect.x;
+            rect.width = w/_palette.Length - 3;
+            foreach (var item in _palette)
+            {
+                GUI.backgroundColor = item;
+                if (GUI.Button(rect, GUIContent.none, "WhiteBackground"))
+                    color = item;
+
+                GUI.backgroundColor = Color.white;
+                rect.x = rect.xMax + 1;
+            }
+
+            EditorGUILayout.EndHorizontal();
+            GUILayout.Space(20);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                foreach (var t in targets)
+                {
+                    Undo.RecordObjects(targets, "Change Divider Color");
+                    ((EditorDivider) t).Color = color;
+                    EditorUtility.SetDirty(t);
+                }
+            }
+
+        }
+    }
+}
---
+++ Editor/Scripts/HierarchyFormat.cs
@@ -0,0 +1,132 @@
+﻿using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace HierarchyExtensions
+{
+    public static class HierarchyFormat
+    {
+        private static GUIStyle _dividerLabelStyle;
+        private static GUIStyle _childLabelStyle;
+        private static string[] _visibleObjs;
+
+        private static Color _backgroundColor => EditorGUIUtility.isProSkin
+            ? new Color32(56, 56, 56, 255)
+            : new Color32(194, 194, 194, 255);
+
+        [InitializeOnLoadMethod]
+        public static void Init()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItem_CB;
+        }
+
+        private static void Test()
+        {
+            Debug.Log("This is a test :)");
+        }
+
+        private static void UpdateVisibleObjects()
+        {
+            // Holy heck this method really sucks - I could not find a callback or anything like onHierarchyItemExpanded.
+            var window = Resources
+                .FindObjectsOfTypeAll<EditorWindow>()
+                .First(x => x.GetType().Name == "SceneHierarchyWindow");
+
+            var type = window.GetType();
+            var method = type.GetMethod("GetCurrentVisibleObjects", BindingFlags.Public | BindingFlags.Instance);
+            var objs = (string[])method?.Invoke(window, null);
+            _visibleObjs = objs;
+        }
+
+        private static void HierarchyWindowItem_CB(int id, Rect rect)
+        {
+            _dividerLabelStyle ??= new GUIStyle(EditorStyles.label) { fontStyle = FontStyle.Bold };
+            _childLabelStyle ??= new GUIStyle(EditorStyles.label);
+
+            var obj = EditorUtility.InstanceIDToObject(id);
+            var gameObject = obj as GameObject;
+
+            if (gameObject == null)
+                return;
+
+            var divider = gameObject.GetComponentInParent<EditorDivider>();
+            if (divider == null)
+                return;
+
+            var isRoot = divider.gameObject == gameObject;
+
+            if (Event.current.type is not EventType.Repaint)
+                return;
+
+            var isSelected = Selection.objects.Contains(obj);
+
+            // Draw the background colour
+            var backgroundRect = new Rect(rect);
+            backgroundRect.x = 32f;
+            backgroundRect.width += 512f;
+            backgroundRect.height -= 2;
+            backgroundRect.y += 1f;
+            GUI.backgroundColor = _backgroundColor;
+            GUI.Box(backgroundRect, GUIContent.none, "OverrideMargin");
+
+            if (isRoot)
+            {
+                GUI.backgroundColor = divider.Color;
+                GUI.Box(backgroundRect, GUIContent.none, "OverrideMargin");
+            }
+            else
+            {
+                GUI.backgroundColor = new Color(divider.Color.r, divider.Color.g, divider.Color.b, 0.1f);
+                GUI.Box(backgroundRect, GUIContent.none, "OverrideMargin");
+            }
+
+            // Draw the selection box overlay
+            if (isSelected)
+            {
+                var selectionColour = new Color(0f, 0.49f, 1f, 0.5f);
+                GUI.backgroundColor = selectionColour;
+                GUI.Box(backgroundRect, GUIContent.none, "OverrideMargin");
+            }
+
+            GUI.backgroundColor = Color.white;
+
+            const float colorFactor = 0.35f;
+            var contentColor = isRoot
+                ? new Color(divider.Color.r*colorFactor, divider.Color.g*colorFactor, divider.Color.b*colorFactor)
+                : Color.white;
+
+            var iconRect = new Rect(rect);
+            iconRect.width = 20;
+            iconRect.height = 20;
+            iconRect.x += -2f;
+            iconRect.y += -2f;
+            GUI.color = contentColor;
+            GUI.Label(iconRect, isRoot
+                ? EditorGUIUtility.IconContent(divider.Icon)
+                : EditorGUIUtility.IconContent("d_GameObject Icon"));
+            GUI.color = Color.white;
+
+            var labelRect = new Rect(rect);
+            labelRect.y -= 2f;
+            labelRect.x += 17f;
+            labelRect.height += 2f;
+            GUI.contentColor = contentColor;
+            GUI.Label(labelRect, gameObject.name, isRoot ? _dividerLabelStyle : _childLabelStyle);
+            GUI.contentColor = Color.white;
+
+            if (gameObject.transform.childCount > 0)
+            {
+                UpdateVisibleObjects();
+                var expanded = !_visibleObjs.Contains(gameObject.transform.GetChild(0).name);
+                var foldoutRect = new Rect(rect);
+                foldoutRect.x -= 16f;
+                GUI.color = contentColor;
+                GUI.Label(foldoutRect, EditorGUIUtility.IconContent(expanded ? "IN foldout act" : "IN foldout act on"));
+                GUI.color = Color.white;
+            }
+
+            EditorApplication.RepaintHierarchyWindow();
+        }
+    }
+}
